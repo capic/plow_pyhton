b1e18722e83a6d987a5ecb1769c5b16e210a6742
capic <vcapic@gmail.com>
2015-07-22 09:42:27 +0200

    Update download_basic.py

--- download_basic.py
+++ download_basic.py
@@ -368,6 +368,7 @@ class ManageDownloads:
 
         if not utils.check_pid(download.pid_plowdown):
             utils.kill_proc_tree(download.pid_python)
+            logging.debug('Process %s for download %s killed for inactivity ...\r\n' % (download.pid_python, download.name))
 
             download.pid_plowdown = 0
             download.pid_python = 0
