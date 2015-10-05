f12caafddca5fdfed87188b67f0fad0b79708a56
capic <vcapic@gmail.com>
2015-10-05 18:16:10 +0200

    add package id

--- main/bean/downloadBean.py
+++ main/bean/downloadBean.py
@@ -59,7 +59,7 @@ class Download:
 
     def to_insert_json(self):
         return {"name": self.name,
-                "package_id": self.package.id,
+                "package_id": self.package.id if self.package is not None else 'null',
                 "link": self.link,
                 "size_file": str(self.size_file),
                 "status": str(self.status),
