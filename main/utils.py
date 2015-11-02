0fdce15b2f632297e60c7a7110fc60d409ced500
capic <vcapic@gmail.com>
2015-11-02 16:32:39 +0100

    manage directories

--- main/utils.py
+++ main/utils.py
@@ -136,7 +136,10 @@ def json_to_download_object(json_object):
     download.time_left = json_object['time_left']
     download.pid_plowdown = json_object['pid_plowdown']
     download.pid_python = json_object['pid_python']
-    download.directory = json_object['directory']
+    download.directory = DownloadDirectory()
+    if json_object['directory_id']:
+        download_package.id = json_object['download_directory']['id']
+        download_package.name = json_object['download_directory']['path']
     download.file_path = json_object['file_path']
     download.priority = json_object['priority']
     if json_object['theorical_start_datetime'] == 0:
