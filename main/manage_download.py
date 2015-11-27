719902c8c52947fee40c9fbdec77506bb81b404a
capic <vcapic@gmail.com>
2015-11-27 20:18:32 +0100

    unrar

--- main/manage_download.py
+++ main/manage_download.py
@@ -547,7 +547,9 @@ def unrar(self, download):
                 download.logs = line
                 values = line.split()
                 if 'stdo' in values[0]:
-                    percent = values[1].replace('%', '')
+                    percent = int(values[1].replace('%', ''))
+                    if not percent.isdigit():
+                        percent = 100
                     self.update_download_directory_unrar_percent(download.directory.id, percent)
 
                 self.update_download_log(download)
