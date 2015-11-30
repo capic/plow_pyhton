f054136eeb7d9d0112a1af22e4a2fc1876a70e09
capic <vcapic@gmail.com>
2015-11-30 12:30:57 +0100

    utilisation de to move directory

--- main/utils.py
+++ main/utils.py
@@ -165,10 +165,11 @@ def json_to_download_object(json_object):
         download_directory.id = json_object['download_directory']['id']
         download_directory.path = json_object['download_directory']['path']
     download.directory = download_directory
+    to_move_download_directory = DownloadDirectory()
     if json_object['to_move_directory_id']:
-        download_directory.id = json_object['to_move_download_directory']['id']
-        download_directory.path = json_object['to_move_download_directory']['path']
-    download.to_move_directory = download_directory
+        to_move_download_directory.id = json_object['to_move_download_directory']['id']
+        to_move_download_directory.path = json_object['to_move_download_directory']['path']
+    download.to_move_directory = to_move_download_directory
     download.file_path = json_object['file_path']
     download.priority = json_object['priority']
     if json_object['theorical_start_datetime'] == 0:
