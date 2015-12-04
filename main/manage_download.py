428a21552cd6da7d49a43ff732cb223821c158d0
capic <vcapic@gmail.com>
2015-12-05 00:07:33 +0100

    changement du caractere de fin pour la ligne de unrar

--- main/manage_download.py
+++ main/manage_download.py
@@ -586,21 +586,29 @@ class ManageDownload:
 
             line = ''
             while True:
+                print('1')
                 out = p.stderr.read(1)
+                print('2')
                 if out == '' and p.poll() is not None:
+                    print('break')
                     break
                 if out != '':
+                    print('3')
                     # if out != '\n' and out != '\r':
                     if out != '%':
+                        print('4')
                         utils.log_debug('out: ' + out)
                         line += out
                     else:
+                        print('5')
                         utils.log_debug('Line %s' % line)
                         download.logs = line
                         if line != '':
+                            print('6')
                             utils.log_debug('1) line %s' % line)
                             values = line.split()
                             if len(values) > 1:
+                                print('7')
                                 utils.log_debug('2) values[0] %s' % values[0])
                                 if 'stdo' in values[0]:
                                     percent = int(values[1].replace('%', ''))
