6997fae01b8bad06658086626daca73b0d9884b0
capic <vcapic@gmail.com>
2015-12-05 11:47:35 +0100

    changement du caractere de fin pour la ligne de unrar

--- main/manage_download.py
+++ main/manage_download.py
@@ -591,7 +591,7 @@ class ManageDownload:
                     break
                 if out != '':
                     #if out != '\n' and out != '\r':
-                    if out != '\n' and out != '\r':
+                    if out != '%' or (out != '\n' and out != '\r'):
                         line += out
                     else:
                         print('Line %s' % line)
