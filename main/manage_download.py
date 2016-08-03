cb1ea5077fa16fd674bacb48850b9a73ff45b2d6
Vincent <vcapic@gmail.com>
2016-08-03 23:19:43 +0200

    correction

--- main/manage_download.py
+++ main/manage_download.py
@@ -382,13 +382,13 @@ class ManageDownload:
         line = ''
         while True:
             out = p.stderr.read(1).decode('utf-8')
-            print(out)
             if out == '' and p.poll() is not None:
                 break
             if out != '':
                 if out != '\n' and out != '\r':
                     line += out
                 else:
+                    print(line)
                     line = utils.clean_plowdown_line(line)
                     download = ManageDownload.get_download_values(line, download)
                     line = ''
