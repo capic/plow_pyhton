d31a8ef5d7e72c5871a40aab71a292c1e06213bc
vcapic@gmail.com <vcapic@gmail.com>
2016-08-03 14:48:26 +0200

    remplacement du not in par find

--- main/utils.py
+++ main/utils.py
@@ -87,7 +87,8 @@ def clean_plowdown_line(line):
 
 def get_infos_plowprobe(cmd):
     log.log('[utils](get_infos_plowprobe) +++', log.LEVEL_DEBUG)
-    output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+    env = { 'PYTHONIOENCODING': 'utf-8' }
+    output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, env=env).communicate()[0].decode("utf-8")
     log.log('[utils](get_infos_plowprobe)  | OUTPUT %s' % output, log.LEVEL_DEBUG)
     if output.find('Link is not alive') == -1:
         if output.startswith('==>'):
