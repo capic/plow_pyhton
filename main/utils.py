5f466f0b337ee32534a6777fc4efc602d7d5ee9d
Vincent <vcapic@gmail.com>
2016-08-03 23:03:37 +0200

    correction

--- main/utils.py
+++ main/utils.py
@@ -153,7 +153,7 @@ def change_action_property(action, attribute_to_find, value_to_find, attribute_t
 
 def json_to_application_configuration_object(json_object):
     log.log('[utils](json_to_application_configuration_object) +++', log.LEVEL_DEBUG)
-    if bool(json_object):
+    if bool(json_object) is not False:
         application_configuration = ApplicationConfiguration()
         application_configuration.id = json_object['id']
         application_configuration.download_activated = json_object['download_activated']
