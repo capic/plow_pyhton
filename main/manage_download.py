2d70540fa3858e7c1cd8fc31a6aeb8137fd9ef77
vcapic@gmail.com <vcapic@gmail.com>
2016-08-03 16:02:51 +0200

    remplacement du not in par find

--- main/manage_download.py
+++ main/manage_download.py
@@ -33,8 +33,6 @@ import inspect
 
 import codecs
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
-sys.stdin = codecs.getwriter('utf-8')(sys.stdin)
-
 
 class ManageDownload:
     COMMAND_DOWNLOAD = "/usr/bin/plowdown -r 10 -x --9kweu=I1QOR00P692PN4Q4669U --temp-rename --temp-directory %s -o %s %s"
