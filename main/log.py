39f7e4092632ca748adcda990a5ec78c38350b6e
vcapic@gmail.com <vcapic@gmail.com>
2016-08-03 10:19:04 +0200

    test unicode print

--- main/log.py
+++ main/log.py
@@ -3,6 +3,10 @@ import utils
 import logging
 import config
 import time
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 LEVEL_OFF = 0
 LEVEL_ALERT = 1
