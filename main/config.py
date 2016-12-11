4656e041143b5ef6d7ae792d1242cd5fdb9ce64a
capic <vcapic@gmail.com>
2016-12-11 21:57:41 +0100

    config prod

--- main/config.py
+++ main/config.py
@@ -5,8 +5,8 @@ from bean.downloadDirectoryBean import DownloadDirectory
 LOG_FORMAT = '[%(levelname)8s]  %(asctime)s <%(to_ihm)4s>     (%(file_name)s) {%(function_name)s} "%(message)s"'
 DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
-# CONFIG_FILE = '/var/www/plow_solution/config_python.cfg'
-CONFIG_FILE = '/var/www/plow_solution_dev/config_python.cfg'
+CONFIG_FILE = '/var/www/plow_solution/config_python.cfg'
+# CONFIG_FILE = '/var/www/plow_solution_dev/config_python.cfg'
 
 RESCUE_MODE = False
 
@@ -17,8 +17,8 @@ def init():
 
     application_configuration.id_application = 1
     application_configuration.download_activated = True
-    application_configuration.rest_address = 'http://192.168.1.101:3001/'
-    # application_configuration.rest_address = 'http://192.168.1.101:3000/'
+    # application_configuration.rest_address = 'http://192.168.1.101:3001/'
+    application_configuration.rest_address = 'http://192.168.1.101:3000/'
     application_configuration.notification_address = 'ws://capic.hd.free.fr:8181/ws'
     application_configuration.python_log_level = 4
     application_configuration.python_log_format = LOG_FORMAT
