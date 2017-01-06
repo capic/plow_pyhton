0ac84642e9c2fc6153645af00eb4fea03f9f88bc
capic <vcapic@gmail.com>
2017-01-06 12:55:50 +0100

    refactor

--- main/config.py
+++ main/config.py
@@ -6,7 +6,7 @@ LOG_FORMAT = '[%(levelname)8s]  %(asctime)s <%(to_ihm)4s>     (%(file_name)s) {%
 DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
 # CONFIG_FILE = '/var/www/plow_solution/plow_python/config_python.cfg'
-CONFIG_FILE = '../config_python.cfg'
+CONFIG_FILE = os.path.dirname(os.path.abspath(__file__)) + '../config_python.cfg'
 
 RESCUE_MODE = False
 
